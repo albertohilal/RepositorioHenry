bfd546ce4439313ce742c70b8c5c9d177d2354ca
albertohilal <albertohilal@github.com>
2024-12-18 18:43:40 -0300

    Control de Flujos

---
+++ CarpetaHenry/JAVASCRIPT/controlDeFlujos.js
@@ -0,0 +1,11 @@
+function viajar(destino) {
+  if (destino === "Brasil") {
+    console.log("doblar a la IZQUIERDA");
+  } else if (destino === "Argentina") {
+    console.log("doblar a la DERECHA");
+  } else {
+    console.log("Estamos PERDIDOS");
+  }
+}
+
+viajar("Bolivia");
